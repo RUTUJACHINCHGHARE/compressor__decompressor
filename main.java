84cf390aaed16a8bf420ed6ef5c9a25f412cc2d2
RUTUJACHINCHGHARE <86714232+RUTUJACHINCHGHARE@users.noreply.github.com>
2023-06-20 10:16:09 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,15 @@
+package GUI;
 
+/**
+ *
+ * @author rutuc
+ */
+public class main {
+    public static void main(String[] args){
+        AppFrame frame = new AppFrame();
+        frame.setTitle("Compressor-Decompressor");
+        
+        
+        
+    }
+}
